7b6e212dadc242da1ab9a127dfe9ef4616b9c29a
utkarshletter <150581222+utkarshletter@users.noreply.github.com>
2025-05-28 14:54:53 +0530

    Update 1

--- Backend/Socket/socket.js
+++ Backend/Socket/socket.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5000'],
+    origin: ['https://uv-chatters.onrender.com/'],
     methods: ["GET", "POST"],
     credentials: true,
   }
