bbee5f9bf86085325d8b3039404ebb70ac67e4a4
Utkarsh Varun <uv2277@gmail.com>
2025-05-28 05:40:30 +0530

    recovered for deployment

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     plugins: [react(),
         tailwindcss(),],
     server:{
-        port: 3000,
+        port: 5000,
         proxy:{
             '/api':{
                 target:'http://localhost:5000'
